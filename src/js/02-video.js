740bfcdda8571f3ee8e25b271efecb1c5318b8ba
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-01-15 16:28:39 +0200

    addfiles

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframeEl = document.getElementById('vimeo-player');
+const player = new Player(iframeEl);
+
+
+
+player.on('timeupdate', throttle(updateTime, 1000));
+
+function updateTime ({ second }) {
+localStorage.setItem(STORAGE_KEY, second);
+}
+
+const time = +localStorage.getItem(STORAGE_KEY);
+
+player.setCurrentTime(time);
 
